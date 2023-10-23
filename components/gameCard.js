37afc35fda36a80d6a4e5bba102c1e1a9fc7e2ba
Toby Warburton <tobywarburton10@gmail.com>
2023-10-23 12:38:56 -0700

    initial file adds

---
+++ components/gameCard.js
@@ -0,0 +1,13 @@
+// card for each clickable box 
+
+{
+    type: 'Word',
+    image: require('../images/Pickachu.png')
+ }
+//  ../images/Pickachu.png replaced with word image file path
+
+{
+    type: 'Face',
+    image: require('../images/Pickachu.png')
+ }
+ //  ../images/Pickachu.png replaced with face image file path
---
+++ pages/gamePage.jsx
@@ -0,0 +1,5 @@
+// this is the page where the card components will be arranged in the game formation
+// also instructions component on top of page
+// scores component go to the side and a 
+// reset button component that will shuffle cards
+
