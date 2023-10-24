52ee54a49a17c0c8355ab1a103f7cb45f15f1e34
Toby Warburton <tobywarburton10@gmail.com>
2023-10-23 18:28:23 -0700

    final game code

--- pages/gamePage.jsx
+++
@@ -1,5 +0,0 @@
-// this is the page where the card components will be arranged in the game formation
-// also instructions component on top of page
-// scores component go to the side and a 
-// reset button component that will shuffle cards
-
