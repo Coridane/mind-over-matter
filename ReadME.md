c96ac2e6f6c0870814139741ac83c36ee733a422
Ismail <63372291+elixit@users.noreply.github.com>
2023-10-25 18:19:48 -0700

    add

--- ReadME.md
+++
@@ -1,7 +0,0 @@
-
-
-
-
-## Credits
-Thank you to Shubham Khatri for their base react memory card game we used as inspiration for our application
-https://javascript.plainenglish.io/building-a-card-memory-game-in-react-e6400b226b8f
---
+++ client/components/home.js
@@ -0,0 +1,14 @@
+// home/HomeComponent.js
+import React from 'react';
+import './home.css'; // Import the CSS file
+
+const HomeComponent = () => {
+  return (
+    <div className="home-container">
+      <h1 className="section-title">Welcome to our Home Page</h1>
+      <div className="featured-item">Featured content goes here</div>
+    </div>
+  );
+};
+
+export default HomeComponent;
