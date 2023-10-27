0f0c8371f1644682049245850249079244ce2502
Toby Warburton <tobywarburton10@gmail.com>
2023-10-26 18:01:46 -0700

    client connection updates

--- client/src/pages/gamePage.js
+++ client/src/pages/gamePage.js
@@ -1,2 +1,21 @@
 import React from "react";
-import Game from '../components/nav';
+import Nav from '../components/nav';
+import Game from '../components/game'
+
+function gamePage () {
+    return (
+    <div>
+        <div className= "navbar">
+            <h1 img="client/public/images/cardback.png">
+            </h1>
+        </div>
+        <div className= "game-container">
+            <p> Game component goes here
+            
+            </p>
+        </div>
+    </div>
+    )
+}
+
+export default gamePage;
