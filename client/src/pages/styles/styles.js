a8066cf6712261d8b0860ced39121fadd71bc7d7
valmg87 <valeriemg87@gmail.com>
2023-10-29 17:18:47 -0500

    homepage

--- client/src/pages/styles/styles.js
+++ client/src/pages/styles/styles.js
@@ -1,10 +1,18 @@
-import styled from 'styled-components';
-import background from 'assets/unsplash.png';
-export const Main = styled.div'
-width: 100%;
-height: 100vh;
-background:url (${background}) center/cover no-repeat;
-display:flex;
-justify-content: center;
-align-items:center;
+/**
+ * Home Layout
+ * This will be the main Layout. (Implemented on the Home page).
+ */
 
+import React from "react";
+import"./styles.css";
+
+
+export default function DefaultLayout({ children }) {
+	return (
+		<div className="main_layout">
+			<NavMenu />
+			{children}
+			<Footer />
+		</div>
+	);
+}
