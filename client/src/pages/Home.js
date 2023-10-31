d31075984c57ea52c0bde3ad49463eb9671651fc
Toby Warburton <tobywarburton10@gmail.com>
2023-10-31 13:34:19 -0700

    pre first heroku commit

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,7 +5,7 @@ function Homepage() {
         /*About Autism with link to page*/
         <div className="container-homepage">
             <h3 className="heading-1"><span>About Autism</span></h3>
-            <p className="about-autism">Autism spectrum disorder <span>ASD</span> is a neurological and developmental disorder that affects how people interact with others, communicate, learn, and behave. Although autism can be diagnosed at any age, it is described as a “developmental disorder” because symptoms generally appear in the first 2 years of life.  To learn more about autism, please visit <a href="https://www.nimh.nih.gov/health/topics/autism-spectrum-disorders-asd#:~:text=Autism%20Spectrum%20Disorder-,Overview,first%202%20years%20of%20life.">here</a>.</p>
+            <p className="about-autism">Autism spectrum disorder <span>ASD</span> is a neurological and developmental disorder that affects how people interact with others, communicate, learn, and behave. Although autism can be diagnosed at any age, it is described as a “developmental disorder” because symptoms generally appear in the first 2 years of life.  To learn more about autism, please visit <a href="https://www.nimh.nih.gov/health/topics/autism-spectrum-disorders-asd#:~:text=Autism%20Spectrum%20Disorder-,Overview,first%202%20years%20of%20life." target="blank">The National Institute of Mental Health</a>.</p>
             <hr />
 
         {/*What steps should you take as a parent?*/}
@@ -20,11 +20,11 @@ function Homepage() {
                 <li>Parent Support</li>
             </ul>
             <p className="steps-link">For more information on what steps parents should take, please visit 
-                <a href="https://steadystridesaba.com/?gad_source=1&gclid=CjwKCAjw-eKpBhAbEiwAqFL0mvr5CdLPeMOg4Bmrd8SscQHtQvReHbevD-Lyt8T67x2AtvSPyXNInBoCAKMQAvD_BwE">here</a>.</p>
+            <a href="https://steadystridesaba.com/?gad_source=1&gclid=CjwKCAjw-eKpBhAbEiwAqFL0mvr5CdLPeMOg4Bmrd8SscQHtQvReHbevD-Lyt8T67x2AtvSPyXNInBoCAKMQAvD_BwE" target="blank"> Steady Strides ABA</a>.</p>
             <hr />
 
         {/*Challenges for parents*/}
-            <h3 className="heading-3"><span>Challenges for Parents of Autistic Children</span></h3>
+        <h3 className="heading-3"><span>Challenges for Parents of Autistic Children</span></h3>
             <p className="challenges">Parents of Autistic children are considered superheroes.  They are the support system and enablers of beautiful individuals with unique abilities.  There are new challenges for the family that come forth daily.  These challenges can be both enjoyable and stressful at times.</p>
             <p className="challenge-types">Some common challenges of parenting an Autistic child include:</p>
             <ul className="challenge-list">
@@ -34,22 +34,21 @@ function Homepage() {
                 <li>Communication Challenges</li>
                 <li>Stigmatism</li>
             </ul>
-            <p className="challenge-link-1">For more information on common challenges of parenting an Autistic child, please visit <a href="https://caliberautism.com/blog/Common-Challenges-of-Parenting-an-Autistic-Child">here</a>.</p>
-            <p className="challenge-link-2">If you are father that is in need of support, please visit this <a href="https://podcasters.spotify.com/pod/show/realunfiltereddadjourneys?fbclid=IwAR0rYAcEEiAmpZQ73TV3XoVfhaBehRfuuwFluD-Whkh5ZdA842t80cQQJqQ">Podcast</a>.</p>
+            <p className="challenge-link-1">For more information on common challenges of parenting an Autistic child, please visit <a href="https://caliberautism.com/blog/Common-Challenges-of-Parenting-an-Autistic-Child" target="blank">Caliber Autism & Pediatric Therapy</a>.</p>
+            <p className="challenge-link-2">If you are father that is in need of support, please visit <a href="https://podcasters.spotify.com/pod/show/realunfiltereddadjourneys?fbclid=IwAR0rYAcEEiAmpZQ73TV3XoVfhaBehRfuuwFluD-Whkh5ZdA842t80cQQJqQ" target="blank">Real Unfiltered Dad Journeys</a>.</p>
             <hr />
 
         {/*Homebased Therapy - Main for game*/} 
             <h3 className="heading-4"><span>Home-Based Therapy Games for Children with Autism</span></h3>
             <p className="home-based-therapy">Mind Over Matter would like to help the Autsim community with fun home-based therapy games that you can do in the comfort of your home.  Please click the tabs below for access to those games.</p>
             <ul className="game-list">
-                <li><a href="/game"><button>Memory Game</button></a></li> {/*Need to add link to page*/} 
-                <li><a href="{/*INSERT PAGE LINK*/}"><button>Mirror Game</button></a></li> {/*Need to add link to page - if needed*/} 
+                <li><a href="/game"><button>Memory Game</button></a></li> {/*Need to add link to page*/}  
             </ul> 
             <hr />
 
         {/*Donation sites and donation link*/}
-            <h3 className="heading-5"><span>Would you like to donate to Autism research or donate to Autsim support?</span></h3>
-            <p className="donation-link-1">If you would like to donate to an organization that supports autism, please visit this site for the most popular donation sites in the country<a href="https://www.appliedbehavioranalysisprograms.com/best-autism-charities/">here</a>.</p>
+        <h3 className="heading-5"><span>Would you like to donate to Autism research or donate to Autsim support?</span></h3>
+            <p className="donation-link-1">If you would like to donate to an organization that supports autism, please visit this site for the most popular donation sites in the country<a href="https://www.appliedbehavioranalysisprograms.com/best-autism-charities/" target="blank"> Applied Behavior Analysis Progerams Guide</a>.</p>
             <p className="option"><span>Or Not Sure Where to Donate?</span></p>
             <p className="donation-link-2">Donate with us and we will choose a donation destination for you.  One hundred percent of the donation will go to that destination, and we will provide proof of that donation to you.</p>
             <a href="https://donate.stripe.com/test_bIYfZT2SobC2bW8fYY" target="_blank"><button>Stripe Donation</button></a>
